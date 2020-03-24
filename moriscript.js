16a9c88bf0fd6148b6e692241b2b741f579a8537
Tiffany Yang <teefyang@Tiffanys-MacBook-Pro.local>
2020-03-24 12:34:29 -0700

    adds visitor objects to extend or transform the member of the source code

--- moriscript.js
+++ moriscript.js
@@ -1,7 +1,30 @@
 module.exports = function(babel) {
   //here using babel to transpile the code
-  let t = babel.types;
+  //Let’s start by adding an ArrayExpression method onto our visitor object.
+  // e.g. if it's of type ArrayExpression, we would want to extend the member of the code with a different method
+  const { types: t } = babel;
   return {
-    visitor: {}
+    visitor: {
+      ArrayExpression: context => {
+        context.replaceWith(
+          t.callExpression(
+            t.memberExpression(t.identifier("mori"), t.identifier("vector")),
+            context.node.elements
+          )
+        );
+      },
+      ObjectExpression: context => {
+        let props = [];
+        context.node.properties.forEach(prop => {
+          props.push(t.stringLiteral(prop.key.name), prop.value);
+        });
+        context.replaceWith(
+          t.callExpression(
+            t.memberExpression(t.identifier("mori"), t.identifier("hashMap")),
+            props
+          )
+        );
+      }
+    }
   };
 };
